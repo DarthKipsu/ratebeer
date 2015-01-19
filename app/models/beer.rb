87331e36352c2e3f8a5b02207fb8dbc113d789cb
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-19 10:28:36 +0200

    add initial ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ActiveRecord::Base
     belongs_to :brewery
+    has_many :ratings
+
+    def average_rating
+        sum = ratings.map(&:score).inject(:+)
+        sum / ratings.count.to_f
+    end
 end
