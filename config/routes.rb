ea8cac5fcf3de528c0f5a9c5ef6a371295194725
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-29 22:56:09 +0200

    add users to ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+
   resources :beers
-
   resources :breweries
-
   resources :users
-
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   resource :session, only: [:new, :create, :delete]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+
+  delete 'signout', to: 'sessions#destroy'
 
   root 'breweries#index'
 
