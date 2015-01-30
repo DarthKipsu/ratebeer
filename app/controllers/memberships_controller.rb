c20e99a37dd819c30e07f9b811c4e8326b139003
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-30 22:43:03 +0200

    do not display clubs where already a member in joining

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -15,7 +15,7 @@ class MembershipsController < ApplicationController
   # GET /memberships/new
   def new
     @membership = Membership.new
-    @beer_clubs = BeerClub.all
+    @beer_clubs = BeerClub.all.select{ |club| !club.users.include? current_user }
   end
 
   # GET /memberships/1/edit
