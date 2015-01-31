fd2d0df6b7b4c124dc002775901f916156fe65fd
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-31 09:29:17 +0200

    add passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
     include AverageRating
 
+    has_secure_password
+
     validates :username, uniqueness: true,
                          length: { in: 3..15 }
+    validates :password, length: { minimum: 4 },
+                         format: { with: /([A-Z]\D*\d|\d\D*[A-Z])/,
+                message: "must contain at least one capital letter and one number" }
 
     has_many :ratings
     has_many :beers, through: :ratings
