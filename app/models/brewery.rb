c3081213a8951eaa646f19a6a635fa7291845496
Verna Koskinen <darth.kipsu@gmail.com>
2015-02-16 19:36:59 +0200

    add active and retired breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,17 +3,14 @@ class Brewery < ActiveRecord::Base
 
     validates :name, presence: true
     validates :year, numericality: { only_integer: true,
-                                     greater_than_or_equal_to: 1042 }
-    validate :year_not_in_future
+                                     greater_than_or_equal_to: 1042,
+                                     less_than_or_equal_to: ->(_) { Time.now.year} }
+
+    scope :active, -> { where active:true }
+    scope :retired, -> { where active:[nil,false] }
 
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
-
-    def year_not_in_future
-        if year.present? && year > Time.now.year
-            errors.add(:year, "can't be in the future")
-        end
-    end
 
     def print_report
         puts name
