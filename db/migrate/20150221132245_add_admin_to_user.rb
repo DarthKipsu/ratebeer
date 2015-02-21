3b082a450d1e980d06a287457c0b9a445f38aa09
Verna Koskinen <darth.kipsu@gmail.com>
2015-02-21 16:07:49 +0200

    fix admin migration for travis

--- db/migrate/20150221132245_add_admin_to_user.rb
+++ db/migrate/20150221132245_add_admin_to_user.rb
@@ -3,6 +3,5 @@ class AddAdminToUser < ActiveRecord::Migration
     add_column :users, :admin, :boolean
 
     User.all.each{ |u| u.update_attribute(:admin, false) }
-    User.first.update_attribute(:admin, true)
   end
 end
